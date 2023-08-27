39e631e3175057b08550a12c92c23759ff2a68f7
kurs <kwp@tlen.pl>
2023-08-27 12:20:35 +0200

    27.08 12:20

---
+++ src/test/java/NavigationTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class NavigationTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod(){
+//        ścieżka do drivera
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+//        inicjalizacja sesji
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void navigationToUrl(){
+        driver.get("http://demo-store.seleniumacademy.com/");
+        Assert.assertEquals(driver.getTitle(),"Madison Island");
+    }
+
+}
