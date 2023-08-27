948ca4d92228051ae84bfe2fa8afd26d06f0dcc7
kurs <kwp@tlen.pl>
2023-08-27 14:14:00 +0200

    27.08 14:13

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+public class SearchTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod(){
+//        ścieżka do drivera
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+//        inicjalizacja sesji
+        driver = new ChromeDriver();
+        driver.get("http://demo-store.seleniumacademy.com/");
+    }
+
+    @Test
+    public void searchProduct(){
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("pillow");
+        WebElement searchButton = driver.findElement(By.className("search-button"));
+        searchButton.click();
+        assertThat(driver.getTitle()).isEqualTo("SEARCH RESULTS FOR 'PILLOW'");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
+}
