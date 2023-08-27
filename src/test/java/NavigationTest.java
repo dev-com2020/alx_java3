635b0a5763bfec5d06125d8d6552b5957409946f
kurs <kwp@tlen.pl>
2023-08-27 12:24:01 +0200

    27.08 12:23

--- src/test/java/NavigationTest.java
+++ src/test/java/NavigationTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,4 +23,8 @@ public class NavigationTest {
         Assert.assertEquals(driver.getTitle(),"Madison Island");
     }
 
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
 }
