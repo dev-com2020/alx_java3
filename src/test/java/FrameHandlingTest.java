ee45b7715f6c0c5d32d1ae725f5fdae6427e647d
dev-com2020 <kwp@tlen.pl>
2023-09-16 10:18:22 +0200

    sobota 10:18

--- src/test/java/FrameHandlingTest.java
+++ src/test/java/FrameHandlingTest.java
@@ -1,4 +1,5 @@
 import org.apache.commons.io.FileUtils;
+import org.bson.BSONEncoder;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -8,8 +9,7 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
 
-public class WindowHandlingTest {
-
+public class FrameHandlingTest {
     WebDriver driver;
 
     @BeforeMethod
@@ -18,24 +18,23 @@ public class WindowHandlingTest {
         System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
 //        inicjalizacja sesji
         driver = new ChromeDriver();
-        driver.get("http://guidebook.seleniumacademy.com/Window.html");
+        driver.get("http://guidebook.seleniumacademy.com/Frames.html");
 
     }
     @Test
-    public void handleWindow() throws IOException {
-        String firstWindow = driver.getWindowHandle();
-        System.out.println("Pierwszy hanlder strony to: " + firstWindow);
+    public void switchBetweenWindow() throws IOException {
+//        pierwsza ramka
+        driver.switchTo().frame(0);
+        WebElement firstField = driver.findElement(By.name("1"));
+        firstField.sendKeys("Jestem ramką pierwszą");
+        driver.switchTo().defaultContent();
 
-        WebElement link = driver.findElement(By.linkText("Google Search"));
-        link.click();
-
-        String secondWindow = driver.getWindowHandle();
-        System.out.println("Drugi hanlder strony to: " + secondWindow);
-        System.out.println("Ilość handlerów:" + driver.getWindowHandles().size());
-
-        driver.switchTo().window(firstWindow);
+//        druga ramka
+        driver.switchTo().frame(1);
+        WebElement secondField = driver.findElement(By.name("2"));
+        secondField.sendKeys("Jestem drugą ramką");
         File screenShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenShot, new File("./target/ekran3.png"));
+        FileUtils.copyFile(screenShot, new File("./target/ekran4.png"));
 
     }
 
