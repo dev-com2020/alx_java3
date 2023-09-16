0df8ae18d93f0c91ae0696883f255cfe9988eb6e
dev-com2020 <kwp@tlen.pl>
2023-09-16 11:38:44 +0200

    sobota 11:38

---
+++ src/test/java/StoreCookieInfo.java
@@ -0,0 +1,57 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class StoreCookieInfo {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+//        ścieżka do drivera
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+//        inicjalizacja sesji
+        driver = new ChromeDriver();
+        driver.get("http://demo-store.seleniumacademy.com/customer/account/login/");
+
+    }
+
+    @Test
+    public void storeCookies(){
+        driver.findElement(By.id("email")).sendKeys("user@seleniumacademy.com");
+        driver.findElement(By.id("pass")).sendKeys("tester");
+        driver.findElement(By.id("send2")).submit();
+
+        File dataFile = new File("./target/browser.data");
+        try {
+            dataFile.delete();
+            dataFile.createNewFile();
+            FileWriter fos = new FileWriter(dataFile);
+            BufferedWriter bos = new BufferedWriter(fos);
+            for (Cookie ck: driver.manage().getCookies()){
+                bos.write((ck.getName()) + ";" + ck.getValue() + ";" + ck.getDomain() + ";"
+                + ck.getPath() + ";" + ck.getExpiry() + ";" + ck.isSecure());
+                bos.newLine();
+            }
+            bos.flush();
+            bos.close();
+            fos.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
+}
